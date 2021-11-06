f567fbee2585500dcdff924ba0914129a2c248d7
Nathalia Assuncao <nathalia.p.lins@gmail.com>
2021-11-06 11:18:54 -0300

    subindo aulas

---
+++ AtividadePratica34/js/create.js
@@ -0,0 +1,10 @@
+window.onload = function(){
+    console.log("Carregando Js ...")  
+}
+
+function salvar(){
+    console.log('Salvando dados');
+    let nome = document.getElementById('nome');
+    nome.valeu= " ";
+    console.log(nome.valeu);
+}
