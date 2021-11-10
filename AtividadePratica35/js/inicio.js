507e8b0da907d764ef5752cc4f6a6a859e842bbe
Nathalia Assuncao <nathalia.p.lins@gmail.com>
2021-11-09 22:29:57 -0300

    Atividade 35- Ainda em andamento

---
+++ AtividadePratica35/js/inicio.js
@@ -0,0 +1,21 @@
+function contatos(){
+    let frame = document.getElementById("frame");
+    frame.src="contatos.html"
+}
+
+function experiencias(){
+    let frame = document.getElementById("frame");
+    frame.src="experiencias.html"
+}
+
+function habilidades(){
+    let frame = document.getElementById("frame");
+    frame.src="habilidades.html"
+}
+
+function resumo(){
+    let frame = document.getElementById("frame");
+    frame.src="resumo.html"
+}
+
+//essas paginas são carregadas ao clicar 
